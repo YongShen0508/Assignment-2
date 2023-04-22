dddcdab87fd275f7a1a4bc962338bb10bedd0c49
yong shen <96345247+YongShen0508@users.noreply.github.com>
2023-04-22 17:22:25 +0800

    Update CodeForText.cpp

--- CodeForText.cpp
+++ CodeForText.cpp
@@ -1,1 +1,36 @@
 
+//declare text 2-D array as global array
+
+string text[20][7];
+
+
+//declare void function for text
+
+void ReadTextRecord(int);
+
+
+
+
+
+
+//define void function
+void ReadTextRecord(int record)
+{
+	ifstream infile;
+	infile.open("title.txt");
+	if (infile.fail())
+		cout << "unable to open the file" << endl;
+	else
+	{
+		while (!infile.eof())
+		{
+			for (int i = 0; i < 19; i++)
+			{
+				for (int j = 0; j < 7; j++)
+				{
+					getline(infile, text[i][j]);
+				}
+			}
+		}
+	}
+}
